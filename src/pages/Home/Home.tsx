039d25e2378e8035114f5532e85bda6dd9816722
muskan <muskanloach984@gmail.com>
2024-10-29 11:39:45 +0530

    about pg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { Footer, LandingPage, Testinomial } from "./components"
+import About from "./components/about"
 
 export const Home: React.FC = () => {
   return (
     <div className="w-full h-screen text-black">
       <LandingPage />
+      <About />
       <Testinomial />
       <Footer />
     </div>
