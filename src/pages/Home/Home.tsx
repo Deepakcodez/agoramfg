6cdfb7acf834b524a15d6f67844bc1feac22289e
muskan <muskanloach984@gmail.com>
2024-10-29 13:52:27 +0530

    fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import {Footer, LandingPage, Testinomial } from "./components"
+import About from "./components/about"
 
 export const Home: React.FC = () => {
   return (
