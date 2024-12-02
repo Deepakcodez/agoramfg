e137fd4f92f6c95524321f01525186d3fe3834a6
Deepak <deepak2603dk@gmail.com>
2024-12-02 22:53:20 +0530

    fix navigation

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,6 +3,5 @@ import { AboutUs } from "./about-us";
 import { Career } from "./careers";
 import { Products } from "./products";
 import { Contact } from "./contact";
-import { Capabilities } from "./capabilities";
 
-export { Home, AboutUs, Career, Products, Contact, Capabilities };
+export { Home, AboutUs, Career, Products, Contact, };
