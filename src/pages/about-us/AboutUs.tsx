d87ffc21f7dd3b5ef7a9ab66a8cf85d1120cef2a
muskan <muskanloach984@gmail.com>
2025-01-28 17:12:53 +0530

    about us page components

--- src/pages/about-us/AboutUs.tsx
+++ src/pages/about-us/AboutUs.tsx
@@ -1,15 +1,31 @@
+import React from "react";
+import { AboutMain } from "./components";
+import { motion, useScroll, useTransform } from "motion/react"; 
+import PageHeroSection from "@/components/PageHeroSection";
 
-import { Footer } from "../Home/components"
-import AboutPage from "../Home/components/AboutPage"
+const Aboutus: React.FC = () => {
+  const { scrollYProgress } = useScroll();
 
-
-const AboutUs = () => {
+  // Transform the scroll progress into a y-axis movement
+  const y = useTransform(scrollYProgress, [0, 1], [-50, -300]);
   return (
-    <>
-    <AboutPage/>
-    <Footer/>
-    </>
-  )
-}
-export default AboutUs
+    <div>
+      <div className="h-fit">
+        <PageHeroSection title={"ABOUT US"} />
+      </div>
+      <div className="relative bg-white md:mx-12 mx-2">
+        {/* Apply the parallax effect to the container */}
+        <motion.div
+          style={{ y }} // Apply the y-axis transformation
+          className="bg-white/60 backdrop-blur-lg rounded-lg border-t shadow-sm border-white w-full p-2"
+        >
+          <AboutMain />
+        </motion.div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Aboutus;
 
---
+++ src/pages/about-us/components/AboutImageSection.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { motion, useInView } from "motion/react";
+import { aboutimage } from "@/constants/imagePath";
+
+const AboutImageSection: React.FC = () => {
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(containerRef, { once: false });
+  return (
+    <div ref={containerRef} className=" relative">
+      <motion.div 
+      initial={{ opacity: 0, y: 30 }}
+        animate={isInView ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 0.5 }} 
+        className="     flex items-center justify-center   ">
+        {isInView && (
+          <img src={aboutimage} alt="image" />  
+        )}
+        {isInView && (
+          <h1 className="absolute text-[4rem] font-bold top-4 -z-30 motion-scale-in-[0.2] motion-translate-x-in-[0%] motion-translate-y-in-[96%] motion-opacity-in-[10%] text-blue-950">
+            ABOUT US
+          </h1>
+        )}
+      </motion.div>
+    </div>
+  );
+};
+
+export default AboutImageSection;
