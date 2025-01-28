8511e7369ad3907b4bf773fd0b14f91762a68cc5
muskan <muskanloach984@gmail.com>
2025-01-28 18:59:39 +0530

    products section

--- src/pages/products/components/ProductAndCategory.tsx
+++ src/pages/products/components/ProductAndCategory.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import Category from './Category'
-import Products from './Products'
+import React, { useState } from "react";
+import Category from "./Category";
+import Products from "./Products";
 
-const ProductAndCategory:React.FC = () => {
+const ProductsWithCategories: React.FC = () => {
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(1); // Default to the first category
+
   return (
-    <div>
-      <Category/>
-      <Products/>
-    </div>
-  )
-}
+    <div className="py-12 px-4 sm:px-8 lg:px-16 xl:px-24">
+      <h1 className="text-4xl font-semibold text-gray-700/90 mb-6">
+        Products
+      </h1>
 
-export default ProductAndCategory
+      {/* Category Selector */}
+      <Category onSelectCategory={(id) => setSelectedCategoryId(id)} />
+
+      {/* Products Grid */}
+      <Products selectedCategoryId={selectedCategoryId} />
+    </div>
+  );
+};
+
+export default ProductsWithCategories;
+
--- src/pages/products/components/Products.tsx
+++ src/pages/products/components/Products.tsx
@@ -1,129 +1,41 @@
 import React from "react";
-import { controlconsole1, controlconsole2, controlconsole3, controlconsole4, controlconsole5, controlconsole6 } from "@/constants/imagePath";
-const Products: React.FC = () => {
-  const products = [
-    {
-      id: 1, image: controlconsole1
-    },
-    {
-      id: 2, image:controlconsole2
-    },
-    
-    {
-      id: 3, image: controlconsole3
+import { productsData } from "./ProductsData";
 
-    },
-    {
-      id: 4,
-
-
-      image:  controlconsole4
-    },
-    {
-      id: 5,
-
-      image: controlconsole5
-
-    },
-    {
-      id: 6,
-
-      image:  controlconsole6
-    },
-    // {
-    //   id: 7,
-
-    //   image: "http://www.leovinyls.com/products/hockenium.jpg",
-
-    // },
-    // {
-    //   id: 8,
-
-    //   image: "http://www.leovinyls.com/products/nimbus_2_new_1.jpg",
-    // },
-    // {
-    //   id: 9,
-
-    //   image: "http://www.leovinyls.com/products/hockenium1.jpg",
-
-    // },
-    // {
-    //   id: 10,
-
-    //   image: "http://www.leovinyls.com/products/royalle_footwear.jpg",
-    // },
-    // {
-    //   id: 11,
-
-    //   image: "http://www.leovinyls.com/products/windermere_footwear.jpg",
-
-    // },
-    // {
-    //   id: 12,
-
-    //   image: "http://www.leovinyls.com/products/cedro.jpg",
-    // },
-    // {
-    //   id: 13,
-
-    //   image: "http://www.leovinyls.com/products/cedro.jpg",
-
-    // },
-    // {
-    //   id: 14,
-
-    //   image: "http://www.leovinyls.com/products/omega_furnishing.jpg",
-    // },
-    // {
-    //   id: 15,
-
-    //   image: "http://www.leovinyls.com/products/bremor_furnishing.jpg",
-
-    // },
-    // {
-    //   id: 16,
-
-    //   image: "http://www.leovinyls.com/products/lama_footware.jpg",
-    // },
-    // {
-    //   id: 17,
-
-    //   image: "http://www.leovinyls.com/products/silkymat_shoes_upholsery.jpg",
-    // },
-    // {
-    //   id: 18,
-
-    //   image: "http://www.leovinyls.com/products/pvc_cloth.jpg",
-    // },
-  ];
+const Products: React.FC<{ selectedCategoryId: number }> = ({
+  selectedCategoryId,
+}) => {
+  const selectedCategory = productsData.find(
+    (cat) => cat.id === selectedCategoryId
+  );
 
   return (
-    <div className="pb-12 px-4 sm:px-8 lg:px-16 xl:px-24">
-      <div className="text-3xl sm:text-4xl font-semibold text-gray-700/90 pb-8 sm:pb-12 flex items-center ">
-        <h1 className="flex items-center ">
-          Products
-
-        </h1>
-
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((product, index) => (
-          <div key={`PRODUCT_${index}`} className="relative group">
-            {/* Image container */}
-            <div className=" h-56 md:h-64 lg:h-72 overflow-hidden rounded-lg relative">
-              {/* Overlay */}
-              <div className="bg-black/50 rounded-2xl md:group-hover:top-[8rem] top-[18rem] z-40 absolute h-full w-full  left-0  duration-300"></div>
-              {/* Image */}
-              <img
-                src={product.image}
-
-                className="w-full h-full object-cover transform scale-125 group-hover:scale-150 transition-transform duration-300"
-              />
-            </div>
-
+    <div className="mt-8">
+      {selectedCategory ? (
+        <div>
+          <h2 className="text-2xl font-semibold text-gray-700 mb-4">
+            {selectedCategory.category}
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {selectedCategory.products.map((product) => (
+              <div
+                key={`PRODUCT_${selectedCategory.id}_${product.id}`}
+                className="relative group"
+              >
+                <div className="h-56 md:h-64 lg:h-72 overflow-hidden rounded-lg relative">
+                  <div className="bg-black/50 rounded-lg absolute inset-0 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                  <img
+                    src={product.image}
+                    className="w-full h-full object-cover transform scale-100 group-hover:scale-110 transition-transform duration-300"
+                    alt={`Product ${product.id}`}
+                  />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ) : (
+        <p className="text-gray-600">No products found for this category.</p>
+      )}
     </div>
   );
 };
